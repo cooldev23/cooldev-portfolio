b398ec886f51acd9834058b608ef24864503000a
cooldev23 <52139107+cooldev23@users.noreply.github.com>
2024-04-08 17:04:39 -0600

    Setting up and using layouts

--- pages/about.vue
+++ pages/about.vue
@@ -2,4 +2,15 @@
   <div>
     <h1>Page about me!</h1>
   </div>
+  <!-- <div><button @click="enableCustomLayout">Update Layout</button></div> -->
 </template>
+
+<script setup>
+// definePageMeta({
+//   layout: 'another'
+// })
+
+// function enableCustomLayout() {
+//   setPageLayout('another')
+// }
+</script>

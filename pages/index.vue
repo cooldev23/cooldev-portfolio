5151be6b349f2f6ea36d695d8f031b67a274c442
src_code_dev <scordill23@gmail.com>
2024-04-09 06:54:41 -0600

    Add meta information

--- pages/index.vue
+++ pages/index.vue
@@ -3,3 +3,31 @@
     <h1>Hello From Index!</h1>
   </div>
 </template>
+
+<script setup>
+useSeoMeta({
+  title: 'Main Page',
+  description: 'Our Main Page',
+  ogTitle: '[og:title]',
+  ogDescription: '[og:description]',
+  ogImage: '[og:image]',
+  ogUrl: '[og:url]',
+  twitterTitle: 'Main Page',
+  twitterDescription: '[twitter:description]',
+  twitterImage: '[twitter:image]',
+  twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>

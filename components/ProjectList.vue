758f1bffd46f2791dfdf9571bb7b601e1dc291e2
cooldev23 <52139107+cooldev23@users.noreply.github.com>
2024-04-09 16:31:20 -0600

    Add page and component

---
+++ components/ProjectList.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h3>Projects</h3>
+    <section v-if="pending">Loading...</section>
+    <section v-else-if="error">Something went wrong... Try again</section>
+    <section v-else>Here is the list of repos</section>
+  </div>
+</template>
+
+<script setup>
+const { error, pending, data } = await useFetch('https://api.github.com/users/cooldev23/repos');
+</script>

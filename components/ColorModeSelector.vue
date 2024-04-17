6a03eaecc87c6bda2d92c5040a8b30d661928f77
src_code_dev <scordill23@gmail.com>
2024-04-16 21:00:05 -0600

    Change width

--- components/ColorModeSelector.vue
+++ components/ColorModeSelector.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <button @click="toggleColorMode" class="ml-5 py-2 px-3 w-36 text-xs bg-blue-100 text-gray-700 hover:bg-blue-300 dark:text-gray-700 rounded shadow-md ">Change to {{ nextMode }} {{ nextModeIcon }}</button>
+    <button @click="toggleColorMode" class="ml-5 py-2 px-3 w-40 text-xs bg-blue-100 text-gray-700 hover:bg-blue-300 dark:text-gray-700 rounded shadow-md ">Change to {{ nextMode }} {{ nextModeIcon }}</button>
   </div>
 </template>
 

a062e586d412eaf68c957be26de0e66911542e12
Sean Cordill <scordill23@gmail.com>
2024-04-20 18:24:15 -0600

    Add dark styling

--- components/content/Card.vue
+++ components/content/Card.vue
@@ -1,6 +1,6 @@
 <!-- components/content/Card.vue -->
 <template>
-  <div class="p-2 border bg-white dark:bg-gray-300 dark:border-gray-700 rounded">
+  <div class="p-2 border bg-white dark:bg-gray-300 dark:border-gray-700">
     <slot />
   </div>
 </template>
